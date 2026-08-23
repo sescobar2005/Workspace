479bc8156a2348947c58bbe915be0dfe5e361069
sescobar2005 <santiagoescobarm2005@gmail.com>
2026-08-22 23:25:44 -0500

    Add palindrome number solution

--- Palindrome number/Main.java
+++ Palindrome number/Main.java
@@ -1,22 +1,26 @@
 class Solution {
-    public boolean isPalindrome(int x){
-        while (true) {
-            int rest = x % 10;
-            int num = x / 10;
-
-            System.out.println(num);
-            System.out.println(rest);
-            return true;
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
         }
-        
+
+        int original = x;
+        long reversed = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reversed;
     }
-    
 }
 
 public class Main {
     public static void main(String[] args) {
         int num = 121;
         boolean flag = new Solution().isPalindrome(num);
-        System.out.println("Hola mundo " + flag);
+        System.out.println(flag);
     }
 }
